d0e478f5b590197b5120886f5808afb473812a7e
IfanID <69411810+IfanID@users.noreply.github.com>
2024-09-30 10:04:50 +0700

    Add files via upload

---
+++ Assets/Js/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAsySLb3qQ65X6ogDsZqE9uCHi7SRYnX7M",
+  authDomain: "kleia-fca9d.firebaseapp.com",
+  databaseURL: "https://kleia-fca9d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "kleia-fca9d",
+  storageBucket: "kleia-fca9d.appspot.com",
+  messagingSenderId: "437165187537",
+  appId: "1:437165187537:web:0d01e91015fa8b519a00f0"
+};
+
+// Inisialisasi Firebase
+const app = firebase.initializeApp(firebaseConfig);
